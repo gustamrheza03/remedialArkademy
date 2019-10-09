e5822ca7a1436da86644570a12bffb0def4fbdf2
gustamrheza03 <guscae4@gmail.com>
2019-10-09 09:04:17 +0700

    remedialArkademy

---
+++ nomor1.js
@@ -0,0 +1,12 @@
+
+<body>
+    <p id="coba"></p>
+    
+    <script>
+    const nama = "Gustam rheza";
+    const hasil1 = nama.length;
+    const hasil2 = hasil1 - 9;
+
+    document.getElementById("coba").innerHTML = hasil2;
+    </script>
+</body>
